def0b1957e399b6a67927762cff48981ff55c82b
Ailwyn0822 <44010933+Ailwyn0822@users.noreply.github.com>
2020-09-25 16:36:46 +0800

    updata

---
+++ app/Http/Controllers/x.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class x extends Controller
+{
+
+    public function fuck(){
+        return view('welcome');
+
+    }
+
+    
+
+
+}
